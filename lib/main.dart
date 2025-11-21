d325b2648ae336ed7bed5eeb851ce14279d16642
yahya <yayagaming239@gmail.com>
2025-11-21 15:13:23 +0200

    return the color to the old one

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyAppState extends State<MyApp> {
                   style: TextStyle(
                     fontSize: 36,
                     fontWeight: FontWeight.bold,
-                    color: Colors.red,
+                    color: Colors.white,
                   ),
                 ),
                 SizedBox(height: 8),
